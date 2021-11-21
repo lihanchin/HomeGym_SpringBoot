796eaf182b4e5b9faceb665c31f116f2423d2281
yuan0123 <bear965791@gmail.com>
2021-11-22 03:26:09 +0800

    fix bug

--- src/main/java/edu/ntut/project_01/homegym/controller/IndexController.java
+++ src/main/java/edu/ntut/project_01/homegym/controller/IndexController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
